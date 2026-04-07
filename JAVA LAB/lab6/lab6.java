ba11514ea589199a079ba04c275c3f449d393ce6
abhishekanand16 <abhishekanand1609@gmail.com>
2026-04-07 10:35:17 +0530

    lab6 java

--- JAVA LAB/lab6/lab6.java
+++ JAVA LAB/lab6/lab6.java
@@ -1,14 +1,18 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 
 public class lab6 extends Frame implements ActionListener {
     private Button button;
     private Label label;
+    private Image backgroundImage;
 
     public lab6() {
         setTitle("Simple AWT Event Handling Example");
-        setSize(300, 200);
+        setSize(500, 300);
         setLayout(new FlowLayout());
+        setBackground(new Color(173, 216, 230));
+        backgroundImage = Toolkit.getDefaultToolkit().getImage("background.jpg");
 
         button = new Button("Click Me");
         button.addActionListener(this);
@@ -25,6 +29,18 @@ public class lab6 extends Frame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         label.setText("Button clicked!");
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if (new File("background.jpg").exists()) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        } else {
+            super.paint(g);
+            return;
+        }
+
+        super.paint(g);
     }
 
     public static void main(String[] args) {
